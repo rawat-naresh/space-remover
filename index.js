ae0507b87611634ba1cb14f9674edb6533fa3ca8
natsu <nareshrawat33@gmail.com>
2018-08-27 14:11:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,3 @@ module.exports = function tiny(string) {
   if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
   return string.replace(/\s/g, "");
 };
-/*This is just another branch of the space remover
-for testing purpose
-*/
-// this is for testing purpose comment added
-
-//make this somethig else
-
-// i tried making by adding this
