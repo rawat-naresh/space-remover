86c179a4925410b20b5d647bff992b66aef7bfd0
Naresh Rawat <nareshrawat33@gmail.com>
2018-08-27 10:15:26 +0530

    gfgfg

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ module.exports = function tiny(string) {
   if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
   return string.replace(/\s/g, "");
 };
+
+//this is modified line
